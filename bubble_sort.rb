f988b1e11033a9e14e86c95f364d7a38d2f477f2
Mark Hahn <markhahn545@gmail.com>
2016-07-15 10:40:19 -0700

    bubble sort method complete

---
+++ bubble_sort.rb
@@ -0,0 +1,20 @@
+
+
+def bubble_sort(input)
+	array = input.split(",")
+	until array == array.sort 
+		array.map.with_index do |number, index|
+			unless array[index+1].to_i==0
+				if array[index].to_i>array[index+1].to_i
+					array.insert(index, array.delete_at(index+1))
+				end
+			end
+		end
+	end
+	
+	puts array
+end
+
+
+bubble_sort(gets)
+
