706eb62061b186cf0a08d4e92a18dab762409921
Mark Hahn <markhahn545@gmail.com>
2016-07-15 13:56:45 -0700

    Completed my_each_with_index, modified formatting for test callings, modified my_each comments as well as established a TDD-esque comment convention

--- enumerable.rb
+++ enumerable.rb
@@ -1,16 +1,37 @@
 module Enumerable
 	def my_each
-		#each recieves one parameter 
-		#iterates over parameter by index
-		#stops iterating when it reaches the end of the object passed into it
-		#sends its original self at the end of iteration
+		#each receives one parameter
+		#each has an iteration counter, which increases with each iteration to the size of the object each is called on
+		#each iterates over the object, one index at a time.
+		#each then prints itself and ends
 	i = 0
 		while i<self.size
 			yield self[i]
 			i += 1
 		end
+		self
 	end
-end
 
-test_array = [1,2,3,4]
-test_array.my_each {|param| puts param.to_i * 3}
+	def my_each_with_index
+		#each_with_idx receives two parameters
+		#each_with_idx has an iteration counter
+		#each_with_idx iterates over the size of the object passed in
+		#each_with_idx yields the value it is currently iterating over, as well as its index, denoted by the iteration counter
+
+		i = 0
+		while i < self.size
+			yield(self[i], i)
+			i += 1
+		end
+	end
+
+end
+		
+
+
+	
+	test_array = [1,2,3,6]
+
+#test_array.my_each {|param| puts param.to_i * 3}
+
+test_array.my_each_with_index {|value, index| puts "#{index}. #{value}"}
